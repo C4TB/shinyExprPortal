3d95346bc661e7d54fd825e03dda10f193b96b25
Rafael Henkin <rafael.henkin@gmail.com>
2022-05-23 16:35:56 +0100

    Use vega heatmap

--- R/utils_vegaplots.R
+++ R/utils_vegaplots.R
@@ -272,3 +272,69 @@ vega_traj_scatterplot <-
       )
     )
   }
+
+
+vega_heatmap <-
+  function(data,
+           x,
+           y,
+           fill,
+           max_pvalue = 0.05,
+           min_corr = 0.25,
+           padj = FALSE) {
+    
+    p_cond <- if (padj) "datum.padj" else "datum.pvalue"
+    
+    text_filter <- 
+      paste(p_cond," <= ",max_pvalue, "&& abs(datum.estimate) >=", min_corr)
+    
+    chart <- list(
+      width <- "container",
+      `$schema` = vega_schema(),
+      data = list(values = data),
+      encoding = list(
+        x = list(
+          field = x,
+          type = "nominal",
+          title = FALSE,
+          axis = list(
+            labelAngle = 0,
+            labelBaseline = "middle",
+            labelFontSize = 11,
+            labelFontWeight = "bold",
+            orient = "top"
+          )
+        ),
+        y = list(
+          field = y,
+          type = "nominal",
+          title = "Gene ranked by lowest p-value",
+          sort = list(field = "pvaluesrank")
+        )),
+      layer = list(
+        list(
+          mark = list(type = "rect"),
+          encoding = list(
+            color = list(
+              field = fill,
+              type = "quantitative",
+              title = "Correlation",
+              scale = list(
+                scheme = "redblue",
+                reverse = TRUE,
+                domain = c(-1, 1)
+              )
+            )
+          )
+        ),
+        list(mark = list(type = "text", tooltip = list(content = "data")),
+             transform = list(
+               list(filter = text_filter)
+             ),
+             encoding = list(
+               text = list(field = "estimate", format = ".2f")
+             ))
+      ),
+      config = list(axis = list(grid = TRUE, tickBand = "extent"))
+    )
+  }
