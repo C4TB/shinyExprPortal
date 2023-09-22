ddcba9310a2330e0b73d758de6113280e226c50a
Rafael Henkin <rafael.henkin@gmail.com>
2023-09-22 17:42:12 +0100

    Increaase number of genes

--- tests/testthat/helper-workflow.R
+++ tests/testthat/helper-workflow.R
@@ -14,12 +14,12 @@ measures_data <- data.frame(
     var_b = c("a", "a", "b", "b")
 )
 
-m <- 100
+m <- 250
 preffix <- do.call(paste0, replicate(4, sample(LETTERS, m, TRUE), FALSE))
 suffix <- do.call(paste0, replicate(3, sample(1:9, m, TRUE), FALSE))
 gene_names <- paste0(preffix, suffix)
 exp_matrix <- matrix(
-    round(stats::rnorm(m * 4)+stats::rpois(m*4, 4), 2),
+    round(stats::rnorm(m * 4)+stats::rpois(m*4, 3), 2),
     nrow = m,
     ncol = 4,
     dimnames = list(gene_names, c("P1", "P2", "P3", "P4"))
